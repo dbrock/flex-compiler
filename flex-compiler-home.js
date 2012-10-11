8194c4ff4d0622dd7b3539a8af505c67e60c3828
Daniel Brockman <daniel@brockman.se>
2012-10-11 16:32:22 +0200

    path.existsSync => fs.existsSync

--- flex-compiler-home.js
+++ flex-compiler-home.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-var path = require("path")
+var fs = require("fs")
 
 module.exports = process.env.FLEX_HOME || process.env.FLEX4_HOME ||
   process.env.FLEX3_HOME || "/usr/local/flex"
 
-if (!path.existsSync(module.exports)) {
+if (!fs.existsSync(module.exports)) {
   console.error(module.exports + ": directory does not exist")
   process.exit(1)
 }
